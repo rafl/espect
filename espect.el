eb9c78cbe0c482e204458bcbaa29a7d76f31978b
Jonathan Rockway <jon@jrock.us>
2010-10-19 17:34:46 -0500

    make interpreter more flexible

--- espect.el
+++ espect.el
@@ -19,27 +19,26 @@
 ;;; Example configuration:
 
 ;; (setq espect-buffer-settings
-;;       '((((:not ((:mode "^gnus") (:mode w3m-mode) (:mode "^erc")
-;;                  (:mode eshell-mode) (:mode term-mode) (:mode gud-mode))))
-;;          (lambda ()
-;;            (highlight-beyond-fill-column)))
-;;         ((:not ((:mode eshell-mode) (:mode term-mode)))
+;;       '(((:not (:mode "^gnus") (:mode w3m-mode) (:mode "^erc")
+;;                (:mode eshell-mode) (:mode term-mode) (:mode gud-mode))
+;;          (lambda () nil))
+;;         ((:not (:mode eshell-mode) (:mode term-mode))
 ;;          (lambda () (hl-line-mode t)))
-;;         (((:mode message-mode)
+;;         ((:mode message-mode)
 ;;           (:mode org-mode)
 ;;           (:mode pod-mode)
 ;;           (:mode markdown-mode)
 ;;           (:mode git-commit-mode)
-;;           (:filename "\\.\\(txt\\|mkn\\)$"))
+;;           (:filename "\\.\\(txt\\|mkn\\)$")
 ;;          (lambda ()
 ;;            (flyspell-mode 1)
 ;;            (auto-fill-mode 1)))
-;;         (((:mode c-mode)
-;;           (:mode cperl-mode)
-;;           (:mode emacs-lisp-mode))
+;;         ((:mode c-mode)
+;;          (:mode cperl-mode)
+;;          (:mode emacs-lisp-mode)
 ;;          (lambda ()
 ;;            (flyspell-prog-mode)))
-;;         (((:project "perl"))
+;;         ((:project "perl")
 ;;          (lambda ()
 ;;            (when (save-excursion
 ;;                    (goto-char (point-min))
@@ -47,80 +46,74 @@
 ;;              (setq
 ;;               tab-width 8
 ;;               indent-tabs-mode t))))
-;;         (((:project "Sub-Name"))
+;;         ((:project "Sub-Name")
 ;;          (lambda ()
 ;;            (setq
 ;;             tab-width 8
 ;;             indent-tabs-mode t
 ;;             c-basic-offset 8)))
-;;         (((:project "Memoize"))
+;;         ((:project "Memoize")
 ;;          (lambda ()
 ;;            (setq
 ;;             tab-width 8
 ;;             indent-tabs-mode t)))
-;;         (((:project "gnus"))
+;;         ((:project "gnus")
 ;;          (lambda ()
 ;;            (setq tab-width 8)))))
 
-
 ;;; Code:
 
-(defvar espect-buffer-settings)
+(defvar espect-rules (make-hash-table)
+  "A hash table matching rule names to a lambda that implements them.")
 
-(defun espect-evaluate-buffer-condition (con)
-  (cond
-   ((functionp con) (funcall con))
-   ((listp con)
-    (cond
-     ((listp (car con))
-      (reduce (lambda (a b) (or a b))
-              (mapcar #'espect-evaluate-buffer-condition con)
-              :initial-value nil))
-     (t
-      (reduce
-       (lambda (a b) (and a b))
-       (let (ret)
-         (while con
-           (let ((k (pop con))
-                 (v (pop con)))
-             (push
-              (cond
-               ((eq k :fun) (funcall v))
-               ((eq k :not)
-                (when (not (listp v)) (error ":not requires a list"))
-                (not (espect-evaluate-buffer-condition v)))
-               ((eq k :mode)
-                (if (stringp v)
-                    (string-match-p v (symbol-name major-mode))
-                  (eq v major-mode)))
-               ((eq k :name)
-                (string-match-p v (buffer-name)))
-               ((eq k :filename)
-                (cond ((and (buffer-file-name) (stringp v))
-                       (string-match-p v (buffer-file-name)))
-                      ((buffer-file-name) v)
-                      (t (not v))))
-               ((eq k :project)
-                ;; assume eproject-maybe-turn-on was called from an earlier hook
-                (cond
-                 ((and eproject-root (stringp v))
-                  (string= v (eproject-name)))
-                 (eproject-root v)
-                 (t (not v))))
-               (t (error "unknown cond")))
-              ret)))
-         ret)
-       :initial-value t))))
-   (t (error "invalid condition"))))
+(defmacro* define-espect-rule (name lambda-list &body code)
+  "Define a rule name NAME that executes CODE."
+  (puthash name `(lambda ,lambda-list ,@code) espect-rules))
+
+(defun espect-eval-rule (name args)
+  "Lookup rule NAME in `espect-rules', and evaluate it against ARGS.
+
+NAME is the symbol to look up.
+ARGS is the argument list to apply to the rule."
+  (let ((rule (gethash name espect-rules)))
+    (when (not rule) (error "No espect rule %s" name))
+    (apply rule args)))
+
+(defun espect-eval-conditions (list)
+  "Evaluate, in order, each rule in LIST with `espect-eval-rule', returning nil as soon as a condition returns nil.  Otherwise return true."
+  (let ((rule (car list))
+        (rest (cdr list)))
+    (and (espect-eval-rule (car rule) (cdr rule))
+         (or (null rest)
+             (espect-eval-conditions rest)))))
+
+(defun espect-eval-script (list) ;; list = (rule rule ... code)
+  (let* ((reversed (reverse list))
+         (code (car reversed))
+         (rules (reverse (cdr reversed))))
+    (message "%s %s" code rules)
+    (when (espect-eval-conditions rules) (funcall code))))
+
+
+(define-espect-rule :not (&rest xs)       (not (espect-eval-conditions xs)))
+(define-espect-rule :fun (fun &rest args) (apply fun args))
+(define-espect-rule :mode (mode)          (cond ((stringp mode) (string-match mode (format "%s" major-mode)))
+                                                ((symbolp mode) (eq mode major-mode))
+                                                (t (error "Mode %s must be a string or symbol!" mode))))
+(define-espect-rule :name (name)          (string-match name (buffer-name)))
+(define-espect-rule :filename (filename)  (string-match filename buffer-file-name))
+(define-espect-rule :project-type (type)  (ignore-errors (string-match type (eproject-type))))
+(define-espect-rule :project-root (root)  (ignore-errors (string-match root (eproject-root))))
+(define-espect-rule :project      (x)     (ignore-errors (or (string-match x (eproject-root))
+                                                             (string-match x (eproject-type)))))
+
+(defvar espect-buffer-settings nil
+  "List of espect scripts to run for every new buffer.")
 
 ;; FIXME: doesn't seem to work entirely when opening new files with emacsclient
 ;; or emacs -nw from the commandline
 (defun espect-apply-buffer-settings (settings)
-  (dolist (setting espect-buffer-settings)
-    (let ((condition (car setting))
-          (action (cadr setting)))
-      (when (espect-evaluate-buffer-condition condition)
-        (funcall action)))))
+  (dolist (script espect-buffer-settings) (espect-eval-script script)))
 
 ;;;###autoload
 (add-hook 'after-change-major-mode-hook
